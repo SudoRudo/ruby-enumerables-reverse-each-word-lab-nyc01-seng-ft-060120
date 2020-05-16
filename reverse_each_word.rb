e97bc5bd3b3ee38cd73d43e23a934d8b61cc8a9b
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-05-16 09:41:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,13 @@
-reverse_each_word
+def reverse_each_word(string)
+  words = %w[string]
+  reversed_words = []
+  i = 0
+  
+  while i < words.length do
+    reversed_words << words[i].reverse
+    i += 1
+  end
+  
+  reversed_words.join(' ')
+  
+end
